db4f700e0df2af6b662a67cb415a1d8b1ebdb5f5
Aleksandr Riabukha <stanleycreamwave@gmail.com>
2021-07-11 12:54:00 +0300

    slick slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,30 +3,11 @@ $('.dropdown-item').on('click', function(){
     $(this).closest('.d_select').find('.dropdown-toggle').text($(this).text());
 });
 
-$('.collapse').collapse();
-
-
-
-$('.customer-slider').owlCarousel({
-    loop: true,
-    margin: 0,
-    nav: false,
-    dots: false,
-    mouseDrag: false,
-    items: 1
+$('.customer-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    // centerMode: true,
+    variableWidth: true
 });
-
-
-// // customer slider
-// $('.customer-slider').owlCarousel({
-//     loop: true,
-//     margin: 0,
-//     nav: true,
-//     dots: false,
-//     mouseDrag: false,
-//     items: 1,
-//     // navText: [
-//     //     '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-//     //     '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-//     // ]
-// });
