4bb1e4fe96952119f2728a4df5a45690ea2c64c5
4EPSI <mnatsakanyan@yual.io>
2021-07-12 15:32:59 +0300

    home mob

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
+// Mobile menu
+$('.burger').on('click', function () {
+    $('.burger').toggleClass('active');
+    $('body').toggleClass('no_scroll');
+    $('.header-menu').toggleClass('active');
+});
+$(document).on('click', function (e) {
+    if (!$(e.target).closest('.header').length) {
+        $('.burger').removeClass('active');
+        $('.header-menu').removeClass('active');
+        $('body').removeClass('no_scroll');
+    }
+    e.stopPropagation();
+});
+
+
 // Dropdown select
 $('.dropdown-item').on('click', function(){
     $(this).closest('.d_select').find('.dropdown-toggle').text($(this).text());
