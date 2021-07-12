0fb660213b29194847233d097963e96d7d9b3a9c
4EPSI <mnatsakanyan@yual.io>
2021-07-12 13:19:38 +0300

    heading content

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,7 @@ $('.customer-slider').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 4,
+    slidesToShow: 3,
     // centerMode: true,
     arrows: false,
     variableWidth: true
